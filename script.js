bdba6a0611f580fed03ae83c114526d7269b3fa7
G-Silvaa <93489073+G-Silvaa@users.noreply.github.com>
2023-05-15 11:40:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
-
 var body = document.querySelector("body");
 
 
@@ -12,4 +11,41 @@ btnSignin.addEventListener("click", function () {
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 })
+
+const text = document.querySelectorAll('.phone h1')
+let idx = 0;
+
+function changeName(){
+    idx++;
+
+    if(idx > text.length - 1)
+        idx = 0;
+
+    text.forEach((sec) => {
+        sec.classList.remove('active')
+    })
+
+    text[idx].classList.add('active')
+    
+
+}
+
+setInterval(changeName, 2000);
+
+const menubar = document.querySelectorAll('ul li a')
+
+function selectL(){
+    menubar.forEach((item)=> 
+        item.classList.remove('cor')
+    )
+    this.classList.add('cor')
+}
+
+menubar.forEach((item)=>
+    item.addEventListener('click',selectL)
+)
+
+
+
+
 
